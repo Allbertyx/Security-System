5c85766971ff1f2d9b8e154fdf053d3e2138d347
Alberto <83698689+Allbertyx@users.noreply.github.com>
2023-04-17 19:04:24 +0200

    Add files via upload

---
+++ flask/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response
+import cv2
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    cap.release()
+
+if __name__ == '__main__':
+    app.run(host='192.168.100.83', port=5000, debug=True)
